3bd918c93c5a33f4c9bdb4b756a4034685d6a95b
AdityaS98 <adityasharmalko1998@gmail.com>
2023-05-25 18:42:30 +0530

    Added : A Programme for calculating fare for multiple cab rides for given time period

--- src/main/java/com/cabincoicegenerator/CabInvoiceGenerator.java
+++ src/main/java/com/cabincoicegenerator/CabInvoiceGenerator.java
@@ -1,10 +1,17 @@
 package com.cabincoicegenerator;
 
+import java.util.*;
+
 public class CabInvoiceGenerator {
+
+    Scanner sc = new Scanner(System.in);
 
     public static final double COST_PER_KM = 10.0;
     public static final double COST_PER_MIN = 1.0;
     public static final double MIN_FARE = 5;
+
+
+
 
     public double calculateFare(double distance , int time){
         double totalFare = (distance * COST_PER_KM) + (time * COST_PER_MIN);
@@ -14,5 +21,22 @@ public class CabInvoiceGenerator {
         return totalFare;
     }
 
+    public double calculateFareOfMultipleRides(double[] distanceCovered,int[] timeTaken){
+        double totalFare = 0;
 
-}
+        for (int i=0;i<distanceCovered.length;i++) {
+            totalFare += calculateFare(distanceCovered[i],timeTaken[i]);
+
+
+        }
+        return totalFare;
+
+
+
+
+
+
+    }
+
+
+
--- src/test/java/CabInvoiceGeneratorTest.java
+++ src/test/java/CabInvoiceGeneratorTest.java
@@ -6,11 +6,21 @@ import org.junit.jupiter.api.Assertions;
 public class CabInvoiceGeneratorTest {
 
     CabInvoiceGenerator invoiceGenerator = new CabInvoiceGenerator();
+
     @Test
-    public void givenDistance_shouldReturnFare(){
+    public void givenDistance_shouldReturnFare() {
         double distance = 2.0;
         int time = 5;
-        double result = invoiceGenerator.calculateFare(distance,time);
-        Assertions.assertEquals(25,result);
+        double result = invoiceGenerator.calculateFare(distance, time);
+        Assertions.assertEquals(25, result);
+    }
+
+    @Test
+    public void givenRides_shouldReturnTotalFare() {
+        double [] distanceCovered={20.5,40.8,100.3};
+        int [] timeTaken = {10,15,20};
+        double result = invoiceGenerator.calculateFareOfMultipleRides(distanceCovered,timeTaken);
+        Assertions.assertEquals(1661.0,result);
+
     }
 }
